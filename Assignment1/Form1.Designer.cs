c9f0be9af2ade0d2b731f66853cd2843857fd649
Osama Sayed <61942698+Osama-Sayed@users.noreply.github.com>
2021-08-03 20:30:59 +0200

    Add files via upload

---
+++ Assignment1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Assignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.L1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(48, 53);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(407, 298);
+            this.dgv1.TabIndex = 0;
+            // 
+            // L1
+            // 
+            this.L1.AutoSize = true;
+            this.L1.Location = new System.Drawing.Point(45, 383);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(0, 17);
+            this.L1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 449);
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.dgv1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DB Viwer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Label L1;
+
+    }
+}
+
---
+++ Assignment1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Threading;
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+
+        static string connectionString = @"Data Source = (local); DataBase = simple; Integrated Security=true";
+        private delegate void SafeCallDelegate(string text);
+        private delegate void SafeCallDelegatedvg(object dgv);
+         //string connectionString = string.Format(Ms, " ");
+        //CallToChildThread
+    /*    public string LableText
+        { 
+        get
+            {
+                return this.L1.Text;
+            }
+            set
+            {
+                this.L1.Text = value;
+            }
+        }
+
+        */
+
+
+        private void WriteTextSafe(string text)
+        {
+            if (L1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                L1.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                L1.Text = text;
+            }
+        }
+
+        private void WritedgvSafe(object ob)
+        {
+            if (dgv1.InvokeRequired)
+            {
+                var d = new SafeCallDelegatedvg(WritedgvSafe);
+                dgv1.Invoke(d, new object[] { ob });
+            }
+            else
+            {
+                dgv1.DataSource = ob;
+            }
+        }
+
+        public void SecondThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Second Thread Call");
+                    using (SqlConnection scon = new SqlConnection(connectionString))
+                    {
+                        scon.Open();
+                        SqlDataAdapter sqlDa = new SqlDataAdapter("Select * from Tb", scon);
+                        DataTable dt = new DataTable();
+                        sqlDa.Fill(dt);
+                        WritedgvSafe(dt);
+
+
+                    }
+                    // the thread is paused for 5000 milliseconds
+                    int sleepfor = 60000;
+
+                    Console.WriteLine("Child Thread Paused for {0} seconds", sleepfor / 1000);
+                    WriteTextSafe("loading");
+                    Thread.Sleep(sleepfor);
+                    Console.WriteLine(" Thread resumes");
+                    WriteTextSafe("Done");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Thread Abort Exception" + e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Couldn't catch the Thread Exception");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ThreadStart childref = new ThreadStart(SecondThread);
+                Console.WriteLine("In Main: Creating the Second thread");
+
+
+             /*   using (SqlConnection scon = new SqlConnection(connectionString))
+                {
+                    scon.Open();
+                    SqlDataAdapter sqlDa = new SqlDataAdapter("Select * from Tb", scon);
+                    DataTable dt = new DataTable();
+                    sqlDa.Fill(dt);
+                    dgv1.DataSource = dt;
+
+                    dgv1.Refresh();
+                }*/
+
+
+                Thread Second = new Thread(childref);
+                Second.Start();
+                //Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(" Exception" + ex.ToString());
+            }
+        }
+    }
+}
